0cf2770ec5e54ef4f902b33e96001361e4273b38
Sam Ball <stmball@gmail.com>
2023-12-28 21:35:46 +0000

    vsn v0.1

---
+++ src/vsn.py
@@ -0,0 +1,189 @@
+"""Variance Stabilizing Normalization class for microarray data."""
+import numpy as np
+import numpy.typing as npt
+from sklearn.base import BaseEstimator, TransformerMixin
+from enum import Enum
+
+
+class F(Enum):
+    """Enum for the function to be used in the VSN."""
+
+    EXP = 1
+    ID = 2
+
+
+class VSN(BaseEstimator, TransformerMixin):
+    """Variance Stabilizing Normalization class for microarray data."""
+
+    def __init__(self, f: F = F.EXP):
+        """Initialize the VSN.
+
+        Args:
+            f (F, optional): Function used in VSN on the gradients (b) before applied
+            to x. A choice of exponential makes calculations easier.
+            Defaults to F.EXP.
+        """
+        self.f = np.exp if f is F.EXP else lambda x: x
+        self.df = np.exp if f is F.EXP else lambda _: 1
+
+    def fit(self, x: npt.ArrayLike, tol: float = 1e-6, max_iter=1000) -> None:
+        """Fit the VSN to the data.
+
+        Args:
+            x (npt.ArrayLike): Input data to be fit.
+            tol (float, optional): Tolerance for gradient descent to stop. Defaults to 1e-6.
+            max_iter (int, optional): Max number of iterations before gradient descent stops. Defaults to 1000.
+        """
+        self.x = x
+        self.a = np.ones(x.shape[0])
+        self.b = np.ones(x.shape[0])
+
+        self._fit(tol, max_iter)
+
+    def transform(self, x: npt.ArrayLike) -> npt.NDArray:
+        """Transform the data using the fitted VSN.
+
+        Args:
+            x (npt.ArrayLike): Input data to be transformed.
+
+        Returns:
+            npt.NDArray: Transformed data.
+        """
+        return self._h(x)
+
+    def fit_transform(self, x: npt.ArrayLike) -> npt.NDArray:
+        """Both fit and transform the data.
+
+        Args:
+            x (npt.ArrayLike): Input data to be fit and transformed.
+
+        Returns:
+            npt.NDArray: Transformed data.
+        """
+        self.fit(x)
+        return self.transform(x)
+
+    def _h(self, x: npt.ArrayLike = None) -> npt.NDArray:
+        """Calculate the h function.
+
+        Args:
+            x (npt.ArrayLike, optional): Data to be input into the function.
+            For transformation, the data is passed directly, but for fitting,
+            the class attribute is used. Defaults to None.
+
+        Returns:
+            npt.NDArray: Output of the h function.
+        """
+        if x is None:
+            x = self.x
+        return np.arcsinh(self._Y())
+
+    def _Y(self) -> npt.NDArray:
+        """Calculate the Y function (the inner function of h).
+
+        Returns:
+            npt.NDArray: Output of Y function
+        """
+        return np.broadcast_to(
+            self.f(self.b[:, None]), self.x.shape
+        ) * self.x + np.broadcast_to(self.a[:, None], self.x.shape)
+
+    def _mu(self) -> npt.NDArray:
+        """Calculate the mean of the data along the columns.
+
+        Returns:
+            npt.NDArray: Mean of the data along the columns.
+        """
+        return np.mean(self.x, axis=0)
+
+    def _sigma(self) -> npt.NDArray:
+        """Calculate the standard deviation of all the data.
+
+        Returns:
+            npt.NDArray: Standard deviation of all the data.
+        """
+        return np.var(self.x)
+
+    def _r(self) -> npt.NDArray:
+        """Calculate the intermediate r function.
+
+        Returns:
+            npt.NDArray: Output of the r function.
+        """
+        return self._h() - self._mu()
+
+    def _A(self) -> npt.NDArray:
+        """Calculate the intermediate A function.
+
+        Returns:
+            npt.NDArray: Output of the A function
+        """
+        return 1 / (np.sqrt(1 + self._Y() ** 2))
+
+    def _big_sigma(self, include_x: bool) -> npt.NDArray:
+        """Calculate the sum of the intermediate sum in the gradient calculations.
+
+        Args:
+            include_x (bool): Include multiplying the result by the input data
+            for the gradient of b.
+
+        Returns:
+            npt.NDArray: Output of the sum of the intermediate sum in the
+            gradient calculations.
+        """
+        to_be_summed = (self._r() / self._sigma() + self._A() * self._Y()) * self._A()
+
+        if include_x:
+            to_be_summed = to_be_summed * self.x
+
+        return np.sum(to_be_summed, axis=1)
+
+    def _delta_a(self) -> npt.NDArray:
+        """Calculate the gradient of a.
+
+        Returns:
+            npt.NDArray: Gradient of a.
+        """
+        return self._big_sigma(include_x=False)
+
+    def _delta_b(self) -> npt.NDArray:
+        """Calculate the gradient of b.
+
+        Returns:
+            npt.NDArray: Gradient of b.
+        """
+        n = self.x.shape[1]
+
+        return -n * (self.df(self.b) / self.f(self.b)) + self.df(
+            self.b
+        ) * self._big_sigma(include_x=True)
+
+    def _update(self) -> None:
+        """Update the parameters a and b."""
+        self.a += self._delta_a()
+        self.b += self._delta_b()
+
+    def _converged(self, tol: float) -> bool:
+        """Decide if the gradient descent has converged.
+
+        Args:
+            tol (float): Tolerance for the gradient descent convergence.
+
+        Returns:
+            bool: True if converged, False otherwise.
+        """
+        return np.all(np.abs(self._delta_a()) < tol) and np.all(
+            np.abs(self._delta_b()) < tol
+        )
+
+    def _fit(self, tol: float, max_iter: int) -> None:
+        """Perform the gradient descent.
+
+        Args:
+            tol (float): Tolerance for the gradient descent convergence.
+            max_iter (int): Max number of iterations before gradient descent stops.
+        """
+        for _ in range(max_iter):
+            self._update()
+            if self._converged(tol):
+                break
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,5 +1,7 @@
-"""Sample tests to make sure everything is working correctly."""
-from src.main import main
+"""Tests for the VSN class."""
+from src.vsn import VSN, F
+import pytest
+import numpy as np
 
 
 def test_pytest():
@@ -7,6 +9,139 @@ def test_pytest():
     assert 2 + 2 == 4
 
 
-def test_import():
-    """Test imports are working correctly."""
-    assert main()
+class TestVSN:
+    """Tests for the VSN class."""
+
+    @pytest.fixture
+    def data(self):
+        """Create example data for testing."""
+        return np.arange(15).reshape(3, 5)
+
+    def test_init(self):
+        """Test the class inits correctly."""
+        vsn = VSN()
+        assert vsn.f is np.exp
+        assert vsn.df is np.exp
+        assert vsn.a is None
+        assert vsn.b is None
+        assert vsn.x is None
+
+    def test_Y(self, data):
+        """Test the Y function."""
+        vsn = VSN()
+        vsn.x = data
+        vsn.a = np.ones(data.shape[0])
+        vsn.b = np.ones(data.shape[0])
+
+        expected = np.exp(np.ones(data.shape)) * data + np.ones(data.shape)
+
+        assert np.allclose(vsn._Y(), expected)
+
+    def test_h(self, data):
+        """Test the h function."""
+        vsn = VSN()
+        vsn.x = data
+        vsn.a = np.ones(data.shape[0])
+        vsn.b = np.ones(data.shape[0])
+
+        assert np.allclose(
+            vsn._h(),
+            np.arcsinh(np.exp(np.ones(data.shape)) * data + np.ones(data.shape)),
+        )
+
+    def test_mu(self, data):
+        """Test the mu function."""
+        vsn = VSN()
+        vsn.x = data
+        assert np.allclose(vsn._mu(), np.array([5, 6, 7, 8, 9]))
+
+    def test_sigma(self, data):
+        """Test the sigma function."""
+        vsn = VSN()
+        vsn.x = data
+        assert np.allclose(vsn._sigma(), 4.320493798938574)
+
+    def test_r(self, data):
+        """Test the r function."""
+        vsn = VSN()
+        vsn.x = data
+        vsn.a = np.ones(data.shape[0])
+        vsn.b = np.ones(data.shape[0])
+        assert np.allclose(
+            vsn._r(),
+            np.array(
+                [
+                    [
+                        [
+                            -4.11862641,
+                            -3.9759804,
+                            -4.43887755,
+                            -5.08959988,
+                            -5.83080647,
+                        ],
+                        [
+                            -1.62524773,
+                            -2.45475297,
+                            -3.30910021,
+                            -4.18196948,
+                            -5.06918075,
+                        ],
+                        [
+                            -0.9678257,
+                            -1.87579932,
+                            -2.79152896,
+                            -3.71380877,
+                            -4.64169384,
+                        ],
+                    ]
+                ]
+            ),
+        )
+
+    def test_A(self, data):
+        """Test the A function."""
+        vsn = VSN()
+        vsn.x = data
+        vsn.a = np.ones(data.shape[0])
+        vsn.b = np.ones(data.shape[0])
+        assert np.allclose(
+            vsn._A(),
+            np.array(
+                [
+                    [0.70710678, 0.25971293, 0.15352065, 0.10858589, 0.08392666],
+                    [0.0683731, 0.05767494, 0.04986804, 0.04392086, 0.03924006],
+                    [0.0354603, 0.03234438, 0.0297316, 0.02750924, 0.02559591],
+                ]
+            ),
+        )
+
+    def test_big_sigma(self, data):
+        """Test the big sigma function."""
+        vsn = VSN()
+        vsn.x = data
+        vsn.a = np.ones(data.shape[0])
+        vsn.b = np.ones(data.shape[0])
+        assert np.allclose(
+            vsn._big_sigma(include_x=False),
+            np.array([-0.21790353, 0.07345058, 0.05822896]),
+        )
+
+    def test_delta_a(self, data):
+        """Test the delta a function."""
+        vsn = VSN()
+        vsn.x = data
+        vsn.a = np.ones(data.shape[0])
+        vsn.b = np.ones(data.shape[0])
+        assert np.allclose(
+            vsn._delta_a(), np.array([-0.21790353, 0.07345058, 0.05822896])
+        )
+
+    def test_delta_b(self, data):
+        """Test the delta b function."""
+        vsn = VSN()
+        vsn.x = data
+        vsn.a = np.ones(data.shape[0])
+        vsn.b = np.ones(data.shape[0])
+        assert np.allclose(
+            vsn._delta_b(), np.array([-5.48577879, -3.93426055, -3.29970523])
+        )
